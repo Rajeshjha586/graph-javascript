1ccf4153b1236a99c154b0c7973160da40eed7bc
Rajesh Jha <jharajesh035@gmail.com>
2025-06-15 08:37:46 +0530

    feat: Add graph remove edge and vertex

--- Graph.js
+++ Graph.js
@@ -15,14 +15,48 @@ class Graph {
       this.adjacencyList[vertex2].push(vertex1);
     }
   }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (vertex) => vertex !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (vertex) => vertex !== vertex1
+      );
+    }
+  }
+
+  removeVertex(vertex) {
+    if (this.adjacencyList[vertex]) {
+      while (this.adjacencyList[vertex].length) {
+        const adjacentVertex = this.adjacencyList[vertex].pop();
+        this.removeEdge(vertex, adjacentVertex);
+      }
+
+      delete this.adjacencyList[vertex];
+    }
+  }
 }
 
 const g = new Graph();
 g.addVertex("A");
 g.addVertex("B");
 g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
 
 g.addEdge("A", "B");
 g.addEdge("B", "C");
+g.addEdge("A", "E");
+g.addEdge("B", "E");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+
+g.removeEdge("A", "B");
+g.removeEdge("B", "C");
+g.removeEdge("E", "F");
+
+g.removeVertex("E");
 
 console.log(g);
