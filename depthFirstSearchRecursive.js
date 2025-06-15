ac742efafeaa95365280a98cf5a500386eb2caf7
Rajesh Jha <jharajesh035@gmail.com>
2025-06-15 17:33:55 +0530

    feat: Add dfs recursive solution

---
+++ depthFirstSearchRecursive.js
@@ -0,0 +1,92 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+    }
+  }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (vertex) => vertex !== vertex2
+      );
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (vertex) => vertex !== vertex1
+      );
+    }
+  }
+
+  removeVertex(vertex) {
+    if (this.adjacencyList[vertex]) {
+      while (this.adjacencyList[vertex].length) {
+        const adjacentVertex = this.adjacencyList[vertex].pop();
+        this.removeEdge(vertex, adjacentVertex);
+      }
+
+      delete this.adjacencyList[vertex];
+    }
+  }
+
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+
+      result.push(vertex);
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+
+    return result;
+  }
+}
+
+const g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+console.log(g);
+
+// Graph {
+//     adjacencyList: {
+//       A: [ 'B', 'C' ],
+//       B: [ 'A', 'D' ],
+//       C: [ 'A', 'E' ],
+//       D: [ 'B', 'E', 'F' ],
+//       E: [ 'C', 'D', 'F' ],
+//       F: [ 'D', 'E' ]
+//     }
+//   }
+
+console.log(g.depthFirstRecursive("A"));
+// [ 'A', 'B', 'D', 'E', 'C', 'F' ]
